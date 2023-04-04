14490646a875e09e8a08092afc95e922bf59a12d
simplecloudservlet <outrosdiasvirao@gmail.com>
2023-04-04 11:08:41 -0300

    Lab3

---
+++ a_lab3/src/ServidorImpl.java
@@ -0,0 +1,57 @@
+/**
+ * Laboratorio 3  
+ * Autor: Lucio Agostinho Rocha
+ * Ultima atualizacao: 04/04/2023
+ */
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ServidorImpl implements IMensagem{
+    
+    public ServidorImpl() {
+                
+    }
+    
+    //Cliente: invoca o metodo remoto 'enviar'
+    //Servidor: invoca o metodo local 'enviar'
+    @Override
+    public Mensagem enviar(Mensagem mensagem) throws RemoteException {
+        Mensagem resposta;
+        try {
+        	System.out.println("Mensagem recebida: " + mensagem.getMensagem());
+			resposta = new Mensagem(parserJSON(mensagem.getMensagem()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			resposta = new Mensagem("{\n" + "\"result\": false\n" + "}");
+		}
+        return resposta;
+    }    
+    
+    public String parserJSON(String json) {
+		String result = json;
+		
+		System.out.println(result);
+
+		return result;
+	}
+    public void iniciar(){
+
+    try {
+            Registry servidorRegistro = LocateRegistry.createRegistry(1099);            
+            IMensagem skeleton  = (IMensagem) UnicastRemoteObject.exportObject(this, 0); //0: sistema operacional indica a porta (porta anonima)
+            servidorRegistro.rebind("servidorFortunes", skeleton);
+            System.out.print("Servidor RMI: Aguardando conexoes...");
+                        
+        } catch(Exception e) {
+            e.printStackTrace();
+        }        
+
+    }
+    
+    public static void main(String[] args) {
+        ServidorImpl servidor = new ServidorImpl();
+        servidor.iniciar();
+    }    
+}
